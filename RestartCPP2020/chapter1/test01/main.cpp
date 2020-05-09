c0afd42807d3d9a556860c13e8ad1c2f79553c49
Xxx159 <2625383606@qq.com>
2020-05-09 13:01:39 +0800

    2020/5/9

---
+++ RestartCPP2020/chapter1/test01/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Trace{
+public:
+    Trace(){flag = 1;}
+    void on(){flag = 1;}
+    void off(){flag =0;}
+    void print(const char* s){
+        if(flag){
+            printf("%s\n",s);
+        }
+    } //加const
+
+private:
+    int flag;
+};
+
+int main()
+{
+
+//    Trace t;
+//    t.print("hello world!");
+    Trace t;
+    t.on();
+
+    t.print("hello world");
+    t.off();
+
+
+    return 0;
+}
+
---
+++ RestartCPP2020/chapter1/test01_c/main.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+static int flag = 1;
+void trace_on(){flag = 1;}
+void trace_off(){flag =0;}
+
+void trace2(char* s){
+    if(flag)
+        printf("%s\n",s);
+}
+
+void trace(char* s)
+{
+    printf("%s\n",s);
+}
+
+
+int main()
+{
+   // trace("hello world");
+    trace2("flag1");
+    trace_off();
+    trace2("flag2");
+
+    return 0;
+}
